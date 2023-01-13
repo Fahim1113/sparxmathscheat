51c79386ebd9aca7cbc2fdb1fbbfc316becda27b
Fahim1113 <79474484+Fahim1113@users.noreply.github.com>
2023-01-13 20:09:10 +0000

    Revert "Update mongodb.js"

--- pages/login.js
+++ pages/login.js
@@ -85,7 +85,7 @@ export default function Home() {
               if (username === "" || password === "")
                 setErr("You cannot leave any fields empty");
               else
-                fetch("/api/login", {
+                fetch("http://localhost:3000/api/login", {
                   method: "POST",
                   body: JSON.stringify({
                     username: username,
@@ -111,7 +111,7 @@ export default function Home() {
             }}
           />
           <Link href={"/register"} className={`${inter.className}`}>
-            Do not have an account? Click this to create one.
+            Don't have an account? Click this to create one.
           </Link>
         </div>
       </main>
